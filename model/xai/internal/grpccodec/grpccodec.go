bb460412f292acf72c6df3fa6b250628672cfe14
Koichi Shiraishi <zchee.io@gmail.com>
2025-11-26 06:09:34 +0900

    model/xai/internal/grpccodec: add grpccodec package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ model/xai/internal/grpccodec/grpccodec.go
@@ -0,0 +1,89 @@
+// Copyright 2025 The tumix Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Package grpccodec provides a gRPC codec that uses vtproto for serialization.
+package grpccodec
+
+import (
+	"google.golang.org/grpc/encoding"
+	"google.golang.org/grpc/mem"
+
+	// Guarantee that the built-in proto is called registered before this one
+	// so that it can be replaced.
+	_ "google.golang.org/grpc/encoding/proto"
+)
+
+func init() {
+	encoding.RegisterCodecV2(&Codec{
+		fallback: encoding.GetCodecV2("proto"),
+	})
+}
+
+type vtprotoMessage interface {
+	MarshalToSizedBufferVT(data []byte) (int, error)
+	UnmarshalVT([]byte) error
+	SizeVT() int
+}
+
+// Name is the name registered for the proto compressor.
+const Name = "proto"
+
+// Codec represents a gRPC [encoding.CodecV2] that uses vtproto for messages that
+// implement the vtprotoMessage interface, and falls back to the built-in proto.
+type Codec struct {
+	fallback encoding.CodecV2
+}
+
+var _ encoding.CodecV2 = (*Codec)(nil)
+
+// Name implements [encoding.CodecV2].
+func (Codec) Name() string { return Name }
+
+// Marshal implements [encoding.CodecV2].
+func (c *Codec) Marshal(v any) (mem.BufferSlice, error) {
+	if m, ok := v.(vtprotoMessage); ok {
+		size := m.SizeVT()
+		if mem.IsBelowBufferPoolingThreshold(size) {
+			buf := make([]byte, 0, size)
+			if _, err := m.MarshalToSizedBufferVT(buf[:size]); err != nil {
+				return nil, err
+			}
+			return mem.BufferSlice{mem.SliceBuffer(buf)}, nil
+		}
+
+		pool := mem.DefaultBufferPool()
+		buf := pool.Get(size)
+		if _, err := m.MarshalToSizedBufferVT((*buf)[:size]); err != nil {
+			pool.Put(buf)
+			return nil, err
+		}
+
+		return mem.BufferSlice{mem.NewBuffer(buf, pool)}, nil
+	}
+
+	return c.fallback.Marshal(v)
+}
+
+// Unmarshal implements [encoding.CodecV2].
+func (c *Codec) Unmarshal(data mem.BufferSlice, v any) error {
+	if m, ok := v.(vtprotoMessage); ok {
+		buf := data.MaterializeToBuffer(mem.DefaultBufferPool())
+		defer buf.Free()
+		return m.UnmarshalVT(buf.ReadOnlyData())
+	}
+
+	return c.fallback.Unmarshal(data, v)
+}
