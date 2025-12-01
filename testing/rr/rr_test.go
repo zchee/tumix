8556cec29ecf9acf447f95a5a629c5070238112d
Koichi Shiraishi <zchee.io@gmail.com>
2025-12-02 04:37:47 +0900

    testing/rr: add self-tests for http/grpc replay
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ testing/rr/rr_test.go
@@ -0,0 +1,159 @@
+// Copyright 2025 The tumix Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rr
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-replayers/httpreplay"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type pingServer struct{}
+
+func (pingServer) Ping(_ context.Context, _ *emptypb.Empty) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
+type pingService interface {
+	Ping(context.Context, *emptypb.Empty) (*emptypb.Empty, error)
+}
+
+func registerPingService(s *grpc.Server) {
+	s.RegisterService(&grpc.ServiceDesc{
+		ServiceName: "test.Ping",
+		HandlerType: (*pingService)(nil),
+		Methods: []grpc.MethodDesc{
+			{
+				MethodName: "Ping",
+				Handler: func(srv any, ctx context.Context, dec func(any) error, interceptor grpc.UnaryServerInterceptor) (any, error) {
+					in := &emptypb.Empty{}
+					if err := dec(in); err != nil {
+						return nil, err
+					}
+					if interceptor == nil {
+						return srv.(pingServer).Ping(ctx, in)
+					}
+					info := &grpc.UnaryServerInfo{
+						Server:     srv,
+						FullMethod: "/test.Ping/Ping",
+					}
+					handler := func(ctx context.Context, req any) (any, error) {
+						return srv.(pingServer).Ping(ctx, req.(*emptypb.Empty))
+					}
+					return interceptor(ctx, in, info, handler)
+				},
+			},
+		},
+		Streams:  []grpc.StreamDesc{},
+		Metadata: "test.proto",
+	}, pingServer{})
+}
+
+func TestNewHTTPClientRecordCreatesReplay(t *testing.T) {
+	t.Parallel()
+
+	orig := *Record
+	*Record = true
+	t.Cleanup(func() { *Record = orig })
+
+	client, cleanup, _ := NewHTTPClient(t, func(r *httpreplay.Recorder) {})
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, _ *http.Request) {
+		_, _ = w.Write([]byte("pong"))
+	})
+	srv := http.Server{Handler: mux}
+	ln := httptestListener(t)
+	t.Cleanup(func() { _ = srv.Shutdown(t.Context()) })
+	go func() {
+		_ = srv.Serve(ln)
+	}()
+
+	req, err := http.NewRequestWithContext(t.Context(), http.MethodGet, "http://"+ln.Addr().String()+"/ping", http.NoBody)
+	if err != nil {
+		t.Fatalf("build request: %v", err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("GET /ping: %v", err)
+	}
+	_ = resp.Body.Close()
+
+	cleanup()
+
+	replay := filepath.Join("testdata", t.Name()+".replay")
+	if _, statErr := os.Stat(replay); statErr != nil {
+		t.Fatalf("expected replay file %s: %v", replay, statErr)
+	}
+}
+
+func TestNewInsecureGRPCConnRecordCreatesReplay(t *testing.T) {
+	orig := *Record
+	*Record = true
+	t.Cleanup(func() { *Record = orig })
+
+	ln := grpcListener(t)
+	s := grpc.NewServer()
+	registerPingService(s)
+	go func() {
+		_ = s.Serve(ln)
+	}()
+	t.Cleanup(s.Stop)
+
+	conn, cleanup := NewInsecureGRPCConn(t, "rr-test", ln.Addr().String())
+
+	ctx := t.Context()
+	if err := conn.Invoke(ctx, "/test.Ping/Ping", &emptypb.Empty{}, &emptypb.Empty{}); err != nil {
+		t.Fatalf("Invoke Ping: %v", err)
+	}
+
+	cleanup()
+
+	replay := filepath.Join("testdata", t.Name()+".replay")
+	if _, statErr := os.Stat(replay); statErr != nil {
+		t.Fatalf("expected replay file %s: %v", replay, statErr)
+	}
+}
+
+func httptestListener(t *testing.T) net.Listener {
+	t.Helper()
+
+	var lc net.ListenConfig
+	ln, err := lc.Listen(t.Context(), "tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("httptest listener: %v", err)
+	}
+	return ln
+}
+
+func grpcListener(t *testing.T) net.Listener {
+	t.Helper()
+
+	var lc net.ListenConfig
+	ln, err := lc.Listen(t.Context(), "tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("grpc listener: %v", err)
+	}
+	return ln
+}
